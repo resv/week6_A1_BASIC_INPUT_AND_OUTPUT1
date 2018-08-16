b781a50cd8b4da747118a98407fe213873b7c582
Adam Kim <akim456@gmail.com>
2018-08-16 00:30:20 -0400

    organized

--- Assignment1_2.java
+++ Assignment1_2.java
@@ -1,7 +1,6 @@
 /*	WEEK 6 ASSIGNMENT 1 - BASIC INPUT AND OUTPUT 1
  * 	BASIC INPUT AND OUTPUT EXERCISE 1
  * 	2.Write a java program to read a file line by line and store it into a variable.
- * 
  */
 
 		
